fd190b7a5a8f41a9d3bd592be06bf3bd7ff813a2
Tim Hilliard <tim.hilliard@acquia.com>
2012-02-03 11:56:22 -0500

    Removed confusing options from settings file.

--- settings-search-proxy.php
+++ settings-search-proxy.php
@@ -6,11 +6,5 @@ $settings = array(
     '127.0.0.1',
   ),
   'acquia_identifier' => '', // The Acquia account identifier. e.g. GTWX-10000
-    
-  // You need to set either the acquia_key and derived_key_salt or just the derived key.
-  // NOTE: Currently only the derived_key method works.
-  'acquia_key' => '', // The Acquia key.
-  'derived_key_salt' => '', // ToDo: pull this value directly from the Acquia network.
-    
   'derived_key' => '', // Run drush php-eval 'echo _acquia_search_derived_key();'
 );
